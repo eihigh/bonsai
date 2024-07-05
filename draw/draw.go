04e70913367d377f3aa6abe35538c0efedaaa9e7
eihigh <eihigh.contact@gmail.com>
2024-07-05 18:29:40 +0900

    wip

--- draw/draw.go
+++ draw/draw.go
@@ -3,7 +3,6 @@ package draw
 import (
 	"image/color"
 
-	"github.com/eihigh/bonsai/iter"
 	"github.com/hajimehoshi/ebiten/v2"
 
 	. "github.com/eihigh/bonsai/geometry"
@@ -17,28 +16,52 @@ type Pipeline struct {
 	// TODO: 各種オプションを持たせる
 }
 
-type Mesh struct {
-	AllVertices iter.Seq[Vertex]
-	AllIndices  iter.Seq[uint16]
+type Helper struct {
+	p      *Pipeline
+	dsts   []*Texture
+	srcs   []*Texture
+	tf     func(Vertex) Vertex
+	affine ebiten.GeoM
+	color  color.Color
 }
 
-type Drawer struct {
-	// 便利な描画メソッドを実現するためによく使う値をとにかくぶち込む
-
-	p    *Pipeline
-	dsts []*Texture
-	srcs []*Texture
-	mesh Mesh // nil なら src の領域を元形状として使う
-	mat3 Mat3
-	clr  color.Color
+func NewHelper(p *Pipeline, dst *Texture, srcs ...*Texture) *Helper {
+	return &Helper{p: p, dsts: []*Texture{dst}, srcs: srcs}
 }
 
-func (p *Pipeline) Draw(dsts, srcs []*Texture, mesh Mesh) {
-	vs := make([]Vertex, 4)
-	iter.Append(vs, mesh.AllVertices)
-	is := make([]uint16, 6)
-	iter.Append(is, mesh.AllIndices)
-	opts := &ebiten.DrawTrianglesOptions{}
-	// TODO: p のオプションを opts に反映
-	dsts[0].DrawTriangles(vs, is, srcs[0], opts)
+func (h *Helper) Draw() {
+	// TODO
 }
+
+func (h *Helper) Translate(p Vec2) {
+	if h.tf != nil {
+		panic("do not use affine and tf at the same time")
+	}
+	h.affine.Translate(p.Xy())
+}
+
+// type Mesh struct {
+// 	AllVertices iter.Seq[Vertex]
+// 	AllIndices  iter.Seq[uint16]
+// }
+//
+// type Drawer struct {
+// 	// 便利な描画メソッドを実現するためによく使う値をとにかくぶち込む
+//
+// 	p    *Pipeline
+// 	dsts []*Texture
+// 	srcs []*Texture
+// 	mesh Mesh // nil なら src の領域を元形状として使う
+// 	mat3 Mat3
+// 	clr  color.Color
+// }
+//
+// func (p *Pipeline) Draw(dsts, srcs []*Texture, mesh Mesh) {
+// 	vs := make([]Vertex, 4)
+// 	iter.Append(vs, mesh.AllVertices)
+// 	is := make([]uint16, 6)
+// 	iter.Append(is, mesh.AllIndices)
+// 	opts := &ebiten.DrawTrianglesOptions{}
+// 	// TODO: p のオプションを opts に反映
+// 	dsts[0].DrawTriangles(vs, is, srcs[0], opts)
+// }
